738e7a1021dd9f21693afb3f847558859e87b229
MiguelCabrera <miguel.cabrera.app@gmail.com>
2017-09-19 21:25:29 -0500

    Primer Commit iniciando con la captura de imagenes.

---
+++ chess_read.m
@@ -0,0 +1,15 @@
+%% Script de pruebas, inicia el mecanismo de cargar imagenes con Matlab
+% @mdark1001 
+% miguel.cabrera.app@gmail.com    
+% 2017/08/09
+%  
+close all;
+clear all;
+
+addpath 'Img/';
+
+img_chess=imread('Img/Selección_001.png'); % leemos la imagen 
+imshow(img_chess); %mostramos la imagen :P 
+
+
+
